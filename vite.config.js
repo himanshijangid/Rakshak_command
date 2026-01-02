abf5a9f69e557f7f752a27ab7f1218a6eeac767f
Himanshi <himanshijangid444@gmail.com>
2026-01-02 21:22:15 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,21 @@
 // export default defineConfig({
 //   plugins: [tailwindcss()],
 // });
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   base: "Rakshak_command",
+//   plugins: [tailwindcss()],
+// });
+
+
+
+
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: "Rakshak_command",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Rakshak_command/" : "/",
   plugins: [tailwindcss()],
-});
+}));
