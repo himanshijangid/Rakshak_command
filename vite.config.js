141fe1a5ab4b90764f77c8a3210b1e22b2018eff
Himanshi <himanshijangid444@gmail.com>
2026-01-02 21:05:15 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [tailwindcss()],
+// });
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "Rakshak_command",
   plugins: [tailwindcss()],
 });
