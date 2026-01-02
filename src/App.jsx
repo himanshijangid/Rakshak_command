33a34088eb4e8afe484020c73befb108314a2ebf
Himanshi <himanshijangid444@gmail.com>
2026-01-02 21:40:35 +0530

    buid

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,4 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/home";
-// import About from "./pages/about";
-// import Contact from "./pages/contact";
-// import MainLayout from "./layout/mainLayout";
-// import Services from "./pages/services";
-// import Guards from "./pages/guards";
-// // import GalleryPage from "./pages/galleryPage";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route element={<MainLayout />}>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="/services" element={<Services />} />
-//           <Route path="/guards" element={<Guards />} />
-//           {/* <Route path="/gallery" element={<Gallery />} /> */}
-
-//         </Route>
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
-import React, { StrictMode } from "react";
+// import React, { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import "aos/dist/aos.css";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+
+
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "aos/dist/aos.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
 );
-
